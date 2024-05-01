cd0b9db6a8bf14605457dba50442095972308947
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-01 16:44:36 +0300

    Log in Screen Done

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,8 +9,125 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final email = TextEditingController();
+  FocusNode email_F = FocusNode();
+  final password = TextEditingController();
+  FocusNode password_F = FocusNode();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(width: 96.w, height: 100.h),
+            Center(
+              child: Image.asset('images/logo.png'),
+            ),
+            SizedBox(height: 120.h),
+            Textfield(email, Icons.email, 'Email', email_F),
+            SizedBox(height: 15.h),
+            Textfield(password, Icons.lock, 'Password', password_F),
+            SizedBox(height: 10.h),
+            Forgot(),
+            SizedBox(height: 10.h),
+            Login(),
+            SizedBox(height: 10.h),
+            Have(),
+            Text(
+              "Signup ",
+              style: TextStyle(
+                  fontSize: 15.sp,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget Have() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            "Don't have an account? ",
+            style: TextStyle(
+              fontSize: 13.sp,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget Login() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: 44.h,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10.r),
+        ),
+        child: Text(
+          'Login',
+          style: TextStyle(
+              fontSize: 23.sp,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget Forgot() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: Text(
+        'Forgot your Password?',
+        style: TextStyle(
+            fontSize: 13.sp, color: Colors.blue, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget Textfield(TextEditingController controller, IconData icon, String type,
+      FocusNode focusNode) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Container(
+        height: 44.h,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5.r),
+        ),
+        child: TextField(
+          style: TextStyle(fontSize: 18.sp, color: Colors.black),
+          controller: controller,
+          focusNode: focusNode,
+          decoration: InputDecoration(
+              hintText: type,
+              prefixIcon: Icon(
+                icon,
+                color: focusNode.hasFocus ? Colors.black : Colors.grey,
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.r),
+                borderSide: BorderSide(color: Colors.black, width: 2.w),
+              )),
+        ),
+      ),
+    );
   }
 }
