d859199ae8e8d17e60665a2ebcdfeda2920e33f5
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-01 22:46:02 +0300

    added an icon and fixed a typo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gibibooks/auth/auth_screen.dart';
 import 'package:gibibooks/screen/login_screen.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ScreenUtilInit(designSize: Size(375, 812), child: LoginScreen()),
+      home: ScreenUtilInit(designSize: Size(375, 812), child: AuthPage()),
     );
   }
 }
