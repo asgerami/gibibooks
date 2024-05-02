0e360b3020ebf3edc11a30fa20c8415e1092b6fc
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-02 20:34:16 +0300

    -Storage Function: saves the uploaded images in the app to firebase

---
+++ lib/data/firebase_service/storage.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageMethod {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> uploadImageToStorage(String name, File file) async {
+    Reference ref = _storage.ref().child(name).child(_auth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(file);
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+}
