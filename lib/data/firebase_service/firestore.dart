19ea74cda7a2507d70e67792ef09c5f213670c11
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-02 20:31:25 +0300

    SetUp Cloud FireStore to cache data the app uses

---
+++ lib/data/firebase_service/firestore.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Firebase_Firestore {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<bool> CreateUser({
+    required String email,
+    required String username,
+    required String Bio,
+    required String profile,
+  }) async {
+    await _firebaseFirestore
+        .collection('users')
+        .doc(_auth.currentUser!.uid)
+        .set({
+      'email': email,
+      'username': username,
+      'bio': Bio,
+      'profile': profile,
+      'followers': [],
+      'following': [],
+    });
+    return true;
+  }
+}
