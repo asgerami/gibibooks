fd53bc9747f04e8d5261f0a6c4b5dcc61d9182e7
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-02 20:35:39 +0300

    Firebase Authentication update

--- lib/data/firebase_service/firebase_auth.dart
+++ lib/data/firebase_service/firebase_auth.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gibibooks/data/firebase_service/firestore.dart';
+import 'package:gibibooks/data/firebase_service/storage.dart';
 import 'package:gibibooks/util/exeption.dart';
 
 class Authentication {
@@ -13,6 +15,7 @@ class Authentication {
     required String Bio,
     required File profile,
   }) async {
+    String URL;
     try {
       if (email.isNotEmpty &&
           password.isNotEmpty &&
@@ -21,6 +24,24 @@ class Authentication {
         if (password == passwordConfirm) {
           await _auth.createUserWithEmailAndPassword(
               email: email.trim(), password: password.trim());
+
+          // upload profile pic to storage
+          if (profile != File('')) {
+            URL = await StorageMethod()
+                .uploadImageToStorage('Profile Picture', profile);
+          } else {
+            URL = '';
+          }
+
+          //get info with firestore
+          await Firebase_Firestore().CreateUser(
+            email: email,
+            username: username,
+            Bio: Bio,
+            profile: URL == ''
+                ? 'https://firebasestorage.googleapis.com/v0/b/gibibook-22974.appspot.com/o/person.png?alt=media&token=bbe58ed1-4b00-4e75-9f0b-7ab652eb0e3b'
+                : URL,
+          );
         } else {
           throw exceptions('password and confirm password should be the same!');
         }
