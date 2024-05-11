13b91a85f0dcd3da38d43ee0f2cbbeeb46809650
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-11 18:23:13 +0300

    Added a Feature: Add Posts to the Firebase (firestore). saves posts for later retrival

---
+++ lib/data/firebase_service/firestor.dart
@@ -0,0 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gibibooks/data/model/usermodel.dart';
+import 'package:gibibooks/util/exeption.dart';
+import 'package:uuid/uuid.dart';
+
+class Firebase_Firestor {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<bool> CreateUser({
+    required String email,
+    required String username,
+    required String bio,
+    required String profile,
+  }) async {
+    await _firebaseFirestore
+        .collection('users')
+        .doc(_auth.currentUser!.uid)
+        .set({
+      'email': email,
+      'username': username,
+      'bio': bio,
+      'profile': profile,
+      'followers': [],
+      'following': [],
+    });
+    return true;
+  }
+
+  Future<Usermodel> getUser({String? UID}) async {
+    try {
+      final user = await _firebaseFirestore
+          .collection('users')
+          .doc(UID != null ? UID : _auth.currentUser!.uid)
+          .get();
+      final snapuser = user.data()!;
+      return Usermodel(
+          snapuser['bio'],
+          snapuser['email'],
+          snapuser['followers'],
+          snapuser['following'],
+          snapuser['profile'],
+          snapuser['username']);
+    } on FirebaseException catch (e) {
+      throw exceptions(e.message.toString());
+    }
+  }
+
+  Future<bool> CreatePost({
+    required String postImage,
+    required String caption,
+    required String location,
+  }) async {
+    var uid = Uuid().v4();
+    DateTime data = new DateTime.now();
+    Usermodel user = await getUser();
+    await _firebaseFirestore.collection('posts').doc(uid).set({
+      'postImage': postImage,
+      'username': user.username,
+      'profileImage': user.profile,
+      'caption': caption,
+      'location': location,
+      'uid': _auth.currentUser!.uid,
+      'postId': uid,
+      'like': [],
+      'time': data
+    });
+    return true;
+  }
+}
