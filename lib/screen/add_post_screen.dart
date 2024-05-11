6d60fcbb25fae9ee12b2a61c11b5ae5ab7742acf
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-11 18:14:59 +0300

    Added a feature: created the page to add posts

---
+++ lib/screen/add_post_screen.dart
@@ -0,0 +1,200 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gibibooks/screen/addpost_text.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  final List<Widget> _mediaList = [];
+  final List<File> path = [];
+  File? _file;
+  int currentPage = 0;
+  int? lastPage;
+  bool _hasMoreImages = true;
+
+  Future<void> _fetchNewMedia() async {
+    if (!_hasMoreImages) return;
+
+    lastPage = currentPage;
+    final PermissionState ps = await PhotoManager.requestPermissionExtend();
+    if (ps.isAuth) {
+      List<AssetPathEntity> album =
+          await PhotoManager.getAssetPathList(type: RequestType.image);
+      List<AssetEntity> media =
+          await album[0].getAssetListPaged(page: currentPage, size: 60);
+
+      for (var asset in media) {
+        if (asset.type == AssetType.image) {
+          final file = await asset.file;
+          if (file != null) {
+            path.add(File(file.path));
+          }
+        }
+      }
+
+      List<Widget> temp = [];
+      for (var asset in media) {
+        temp.add(
+          FutureBuilder(
+            future: asset.thumbnailDataWithSize(ThumbnailSize(200, 200)),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Container(
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Image.memory(
+                          snapshot.data!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return Container();
+            },
+          ),
+        );
+      }
+
+      setState(() {
+        _mediaList.addAll(temp);
+        currentPage++;
+        // Check if there are more images to fetch (update _hasMoreImages)
+        if (media.length < 60) {
+          _hasMoreImages = false;
+        }
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchNewMedia();
+  }
+
+  int indexx = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          'New Post',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: false,
+        actions: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
+              child: GestureDetector(
+                onTap: () {
+                  if (_file != null) {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => AddPostTextScreen(_file!),
+                    ));
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text('No Image Selected'),
+                        content: const Text(
+                            'You did not select an image. Please select an image before proceeding.'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                },
+                child: Text(
+                  'Next',
+                  style: TextStyle(
+                    fontSize: 15.sp,
+                    color: const Color.fromRGBO(94, 23, 235, 1),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 375.h,
+                  child: GridView.builder(
+                    itemCount: _mediaList.isEmpty ? _mediaList.length : 1,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 1,
+                      crossAxisSpacing: 1,
+                    ),
+                    itemBuilder: (context, index) {
+                      return _mediaList[indexx];
+                    },
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40.h,
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 10.w),
+                      Text(
+                        'Recent',
+                        style: TextStyle(
+                            fontSize: 15.sp, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: _mediaList.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 1,
+                    crossAxisSpacing: 2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          indexx = index;
+                          _file = path[index];
+                        });
+                      },
+                      child: _mediaList[index],
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
