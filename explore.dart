967932a502a4b16f2e75e5a39d9260f0c87149e0
Amiz-7 <168717733+Amiz-7@users.noreply.github.com>
2024-05-11 20:29:08 +0300

    Create explore.dart
    Created the explore page of the app. this page will be used to search and find other users on the platform through a search box

---
+++ explore.dart
@@ -0,0 +1,15 @@
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final search = TextEditingController();
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  bool show = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold()
