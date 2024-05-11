767d00f8bfb2988154358f10c5b68894b1bf1da0
Amiz-7 <168717733+Amiz-7@users.noreply.github.com>
2024-05-11 18:34:30 +0300

    Get Posts from FireBase
    Added a feature that allows this page to retrieve posts and other things like likes and comment from firebase

--- lib/data/firebase_service/firestor.dart
+++ lib/data/firebase_service/firestor.dart
@@ -69,3 +69,105 @@ class Firebase_Firestor {
     return true;
   }
 }
+
+  Future<bool> CreatReels({
+    required String video,
+    required String caption,
+  }) async {
+    var uid = Uuid().v4();
+    DateTime data = new DateTime.now();
+    Usermodel user = await getUser();
+    await _firebaseFirestore.collection('reels').doc(uid).set({
+      'reelsvideo': video,
+      'username': user.username,
+      'profileImage': user.profile,
+      'caption': caption,
+      'uid': _auth.currentUser!.uid,
+      'postId': uid,
+      'like': [],
+      'time': data
+    });
+    return true;
+  }
+
+  Future<bool> Comments({
+    required String comment,
+    required String type,
+    required String uidd,
+  }) async {
+    var uid = Uuid().v4();
+    Usermodel user = await getUser();
+    await _firebaseFirestore
+        .collection(type)
+        .doc(uidd)
+        .collection('comments')
+        .doc(uid)
+        .set({
+      'comment': comment,
+      'username': user.username,
+      'profileImage': user.profile,
+      'CommentUid': uid,
+    });
+    return true;
+  }
+
+  Future<String> like({
+    required List like,
+    required String type,
+    required String uid,
+    required String postId,
+  }) async {
+    String res = 'some error';
+    try {
+      if (like.contains(uid)) {
+        _firebaseFirestore.collection(type).doc(postId).update({
+          'like': FieldValue.arrayRemove([uid])
+        });
+      } else {
+        _firebaseFirestore.collection(type).doc(postId).update({
+          'like': FieldValue.arrayUnion([uid])
+        });
+      }
+      res = 'seccess';
+    } on Exception catch (e) {
+      res = e.toString();
+    }
+    return res;
+  }
+
+  Future<void> flollow({
+    required String uid,
+  }) async {
+    DocumentSnapshot snap = await _firebaseFirestore
+        .collection('users')
+        .doc(_auth.currentUser!.uid)
+        .get();
+    List following = (snap.data()! as dynamic)['following'];
+    try {
+      if (following.contains(uid)) {
+        _firebaseFirestore
+            .collection('users')
+            .doc(_auth.currentUser!.uid)
+            .update({
+          'following': FieldValue.arrayRemove([uid])
+        });
+        await _firebaseFirestore.collection('users').doc(uid).update({
+          'followers': FieldValue.arrayRemove([_auth.currentUser!.uid])
+        });
+      } else {
+        _firebaseFirestore
+            .collection('users')
+            .doc(_auth.currentUser!.uid)
+            .update({
+          'following': FieldValue.arrayUnion([uid])
+        });
+        _firebaseFirestore.collection('users').doc(uid).update({
+          'followers': FieldValue.arrayUnion([_auth.currentUser!.uid])
+        });
+      }
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+  }
+}
+
