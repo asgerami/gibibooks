2ad2612a84458c5eb6bde87e673f46b4a2fd0502
Amiz-7 <168717733+Amiz-7@users.noreply.github.com>
2024-05-11 20:21:59 +0300

    Created home page
    A page that displays a feed of posts from a Firestore collection, ordered by time, and uses a custom PostWidget to render individual posts.

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gibibooks/widgets/post_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +12,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: SizedBox(
+          width: 145.w,
+          height: 34.h,
+          child: Image.asset('images/gibibook.jpg'),
+        ),
+        backgroundColor: const Color(0xffFAFAFA),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          StreamBuilder(
+            stream: _firebaseFirestore
+                .collection('posts')
+                .orderBy('time', descending: true)
+                .snapshots(),
+            builder: (context, snapshot) {
+              return SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    if (!snapshot.hasData) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    return PostWidget(snapshot.data!.docs[index].data());
+                  },
+                  childCount:
+                      snapshot.data == null ? 0 : snapshot.data!.docs.length,
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
 }
