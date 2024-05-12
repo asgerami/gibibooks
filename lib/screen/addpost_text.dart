5a45cab350cf8e6993c2cd2fdf12826666b1a6d6
Amiz-7 <168717733+Amiz-7@users.noreply.github.com>
2024-05-12 11:36:11 +0300

    Update addpost_text.dart
    Added a feature: it displays a loading indicator  with a Circular Progress Indicator indicating the upload is in progress.

--- lib/screen/addpost_text.dart
+++ lib/screen/addpost_text.dart
@@ -54,6 +54,73 @@ class _AddPostTextScreenState extends State<AddPostTextScreen> {
                 child: Text(
                   'Share',
                   style: TextStyle(color: Colors.blue, fontSize: 15.sp),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: islooding
+            ? const Center(
+                child: CircularProgressIndicator(
+                color: Colors.black,
+              ))
+            : Padding(
+                padding: EdgeInsets.only(top: 10.h),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 65.w,
+                            height: 65.h,
+                            decoration: BoxDecoration(
+                              color: Colors.amber,
+                              image: DecorationImage(
+                                image: FileImage(widget._file),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10.w),
+                          SizedBox(
+                            width: 280.w,
+                            height: 60.h,
+                            child: TextField(
+                              controller: caption,
+                              decoration: const InputDecoration(
+                                hintText: 'Write a caption ...',
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Divider(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.w),
+                      child: SizedBox(
+                        width: 280.w,
+                        height: 30.h,
+                        child: TextField(
+                          controller: location,
+                          decoration: const InputDecoration(
+                            hintText: 'Add University',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      ),
     );
   }
 }
