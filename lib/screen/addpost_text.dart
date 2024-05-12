a3f20175d722e2dfb7fc41f7ce6390097bb000d4
Amiz-7 <168717733+Amiz-7@users.noreply.github.com>
2024-05-12 11:23:27 +0300

    Update addpost_text.dart
    Added code to allows users to create a new post by uploading an image, entering a caption and location, and saving the post data to Firebase Firestore.

--- lib/screen/addpost_text.dart
+++ lib/screen/addpost_text.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gibibooks/data/firebase_service/firestor.dart';
+import 'package:gibibooks/data/firebase_service/storage.dart';
+
 class AddPostTextScreen extends StatefulWidget {
   File _file;
   AddPostTextScreen(this._file, {super.key});
@@ -12,4 +19,41 @@ class _AddPostTextScreenState extends State<AddPostTextScreen> {
   bool islooding = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'New post',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: false,
+        actions: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
+              child: GestureDetector(
+                onTap: () async {
+                  setState(() {
+                    islooding = true;
+                  });
+                  String post_url = await StorageMethod()
+                      .uploadImageToStorage('post', widget._file);
+                  await Firebase_Firestor().CreatePost(
+                    postImage: post_url,
+                    caption: caption.text,
+                    location: location.text,
+                  );
+                  setState(() {
+                    islooding = false;
+                  });
+                  // ignore: use_build_context_synchronously
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Share',
+                  style: TextStyle(color: Colors.blue, fontSize: 15.sp),
+    );
+  }
+}
