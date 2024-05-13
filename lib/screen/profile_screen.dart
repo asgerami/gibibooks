786b03ff8424cea76afcf870f007de5ff754f5dc
Binaa10 <168763214+Binaa10@users.noreply.github.com>
2024-05-14 00:35:39 +0300

    Update profile_screen.dart
    Enabled retrieval of the user's previous posts  from the FireStore on Firebase. it also retrieves the amount of posts, followers and following of the user account from the FireStore and updates the profile

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,7 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gibibooks/data/firebase_service/firestor.dart';
 import 'package:gibibooks/data/model/usermodel.dart';
+import 'package:gibibooks/screen/post_screen.dart';
 import 'package:gibibooks/util/image_cached.dart';
+import 'package:gibibooks/widgets/post_widget.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -13,6 +18,37 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  int post_lenght = 0;
+  bool yourse = false;
+  List following = [];
+  bool follow = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getdata();
+    if (widget.Uid == _auth.currentUser!.uid) {
+      setState(() {
+        yourse = true;
+      });
+    }
+  }
+
+  getdata() async {
+    DocumentSnapshot snap = await _firebaseFirestore
+        .collection('users')
+        .doc(_auth.currentUser!.uid)
+        .get();
+    following = (snap.data()! as dynamic)['following'];
+    if (following.contains(widget.Uid)) {
+      setState(() {
+        follow = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -33,6 +69,40 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   },
                 ),
               ),
+              StreamBuilder(
+                stream: _firebaseFirestore
+                    .collection('posts')
+                    .where('uid', isEqualTo: widget.Uid)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return SliverToBoxAdapter(
+                        child:
+                            const Center(child: CircularProgressIndicator()));
+                  }
+                  post_lenght = snapshot.data!.docs.length;
+                  return SliverGrid(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      final snap = snapshot.data!.docs[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => PostScreen(snap.data())));
+                        },
+                        child: CachedImage(
+                          snap['postImage'],
+                        ),
+                      );
+                    }, childCount: post_lenght),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 4,
+                      mainAxisSpacing: 4,
+                    ),
+                  );
+                },
+              ),
             ],
           ),
         ),
@@ -41,105 +111,181 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
 // Widget Head(Usermodel user) in ProfileScreen class
+// Widget Head(Usermodel user) in ProfileScreen class
   Widget Head(Usermodel user) {
     return Container(
-        color: Colors.white,
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center, // Center align items
-            children: [
-              // Profile picture as a square
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    vertical: 20.h), // Adjust vertical padding as needed
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center, // Center align items
+        children: [
+          // Profile picture as a square
+          Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: 20.h), // Adjust vertical padding as needed
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 80.w,
+                  height: 80.h,
+                  child: CachedImage(user.profile),
+                ),
+                SizedBox(
+                    width: 10
+                        .w), // Adjust spacing between profile picture and username
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(
-                      width: 80.w,
-                      height: 80.h,
-                      child: CachedImage(user.profile),
+                    Text(
+                      user.username,
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    SizedBox(
-                        width: 10
-                            .w), // Adjust spacing between profile picture and username
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          user.username,
-                          style: TextStyle(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 5.h),
-                        Text(
-                          user.Bio,
-                          style: TextStyle(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ],
+                    SizedBox(height: 5.h),
+                    Text(
+                      user.Bio,
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w300,
+                      ),
                     ),
                   ],
                 ),
-              ),
-              // Post, followers, and following count centered
-              Row(
-                mainAxisAlignment:
-                    MainAxisAlignment.center, // Center align row items
+              ],
+            ),
+          ),
+          // Post, followers, and following count centered
+          Row(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Center align row items
+            children: [
+              Column(
                 children: [
-                  Column(
-                    children: [
-                      Text(
-                        'Moments',
-                        style: TextStyle(
-                          fontSize: 13.sp,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    post_lenght.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.sp,
+                    ),
                   ),
-                  SizedBox(
-                      width: 80.w), // Adjust spacing between counts as needed
-                  Column(
-                    children: [
-                      Text(
-                        user.followers.length.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.sp,
-                        ),
-                      ),
-                      Text(
-                        'Followers',
-                        style: TextStyle(
-                          fontSize: 13.sp,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                      width: 60.w), // Adjust spacing between counts as needed
-                  Column(
-                    children: [
-                      Text(
-                        user.following.length.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.sp,
-                        ),
-                      ),
-                      Text(
-                        'Buddies',
-                        style: TextStyle(
-                          fontSize: 13.sp,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    'Moments',
+                    style: TextStyle(
+                      fontSize: 13.sp,
+                    ),
                   ),
                 ],
               ),
-            ]));
+              SizedBox(width: 80.w), // Adjust spacing between counts as needed
+              Column(
+                children: [
+                  Text(
+                    user.followers.length.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.sp,
+                    ),
+                  ),
+                  Text(
+                    'Followers',
+                    style: TextStyle(
+                      fontSize: 13.sp,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(width: 60.w), // Adjust spacing between counts as needed
+              Column(
+                children: [
+                  Text(
+                    user.following.length.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.sp,
+                    ),
+                  ),
+                  Text(
+                    'Buddies',
+                    style: TextStyle(
+                      fontSize: 13.sp,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 20.h),
+          // Unfollow / Message buttons
+          Visibility(
+            visible: follow,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 13.w),
+              child: Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center align row items
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Firebase_Firestor().flollow(uid: widget.Uid);
+                        setState(() {
+                          follow = false;
+                        });
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 30.h,
+                        width: 100.w,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(5.r),
+                          border: Border.all(color: Colors.grey.shade200),
+                        ),
+                        child: Text('Unfollow'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8.w),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 30.h,
+                      width: 100.w,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        borderRadius: BorderRadius.circular(5.r),
+                        border: Border.all(color: Colors.grey.shade200),
+                      ),
+                      child: Text(
+                        'Message',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 5.h),
+          SizedBox(
+            width: double.infinity,
+            height: 30.h,
+            child: const TabBar(
+              unselectedLabelColor: Colors.grey,
+              labelColor: Colors.black,
+              indicatorColor: Colors.black,
+              tabs: [
+                Icon(Icons.image),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 5.h,
+          )
+        ],
+      ),
+    );
   }
 }
