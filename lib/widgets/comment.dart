982490685fb696d717a2162b9f737210b03586fe
abudi-09 <156515083+abudi-09@users.noreply.github.com>
2024-05-14 16:58:35 +0300

    Create comment.dart

---
+++ lib/widgets/comment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Comment extends StatefulWidget {
+  Comment({super.key});
+
+  @override
+  State<Comment> createState() => _CommentState();
+}
+
+class _CommentState extends State<Comment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
