e47f95cea3813616d03788543e33f90f6752e4c3
abudi-09 <156515083+abudi-09@users.noreply.github.com>
2024-05-14 15:48:44 +0300

    Update search.dart
    Constructed a search bar on the top of the page. The search bar allows users to search for other users on the platform.
    
    Uses a CustomScrollView with a SliverToBoxAdapter to position the search bar.
    Implemented the search bar using a Container with a Row containing an icon and a TextField.
    Styled the search bar with a grey background and a thin border.

--- lib/screen/search.dart
+++ lib/screen/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ExploreScreen extends StatefulWidget {
   const ExploreScreen({Key? key}) : super(key: key);
@@ -8,10 +9,50 @@ class ExploreScreen extends StatefulWidget {
 }
 
 class _ExploreScreenState extends State<ExploreScreen> {
-  final search = TextEditingController();
-  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
-  bool show = true;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: CustomScrollView(slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              child: Container(
+                width: double.infinity,
+                height: 36.0,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 10.0),
+                      Expanded(
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            hintText: 'Search User',
+                            hintStyle: TextStyle(color: Colors.black),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ])));
+  }
+}
