8874c9c0087547526925af9ad639dc2755203fb8
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2024-05-15 10:48:48 +0300

    Created User Model File
    implements the Usermodel class to store user information like email, username, bio, profile picture, following list, and follower list.

---
+++ lib/data/model/usermodel.dart
@@ -0,0 +1,10 @@
+class Usermodel {
+  String email;
+  String username;
+  String Bio;
+  String profile;
+  List following;
+  List followers;
+  Usermodel(this.Bio, this.email, this.followers, this.following, this.profile,
+      this.username);
+}
